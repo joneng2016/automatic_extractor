ba4be851ab1b71438f13a513da0da383c32ff2d7
Jonathan Samara <jonathan_samara@hotmail.com>
2022-09-10 19:06:29 -0300

    quase finalizando o código

--- ProcessElement.js
+++ ProcessElement.js
@@ -7,7 +7,8 @@ export default class ProcessElement {
         this.positionOfFile = `${process.cwd()}/readfiles`
         this.filesPosition = []
         this.relationNameFile = [];
-        this.generalData = [] 
+        this.generalData = []
+        this.documentContent = '' 
     }
 
     startProcess() {
@@ -19,24 +20,27 @@ export default class ProcessElement {
                             .map(e => {
 
                                 return {
-                                    nameOfStudent:fs.readFileSync(`${this.positionOfFile}/${e}`,'utf8')
-                                                                    .split('\n')[0]
-                                                                    .split('(')[0]
-                                                                    .split(':')[1]
-                                                                    .trim(),
-                                    nameOfFile:e.split('.')[0]
+                                    nameOfStudent: this._contentOfFile(e)
+                                                            .split('\n')[0]
+                                                            .split('(')[0]
+                                                            .split(':')[1]
+                                                            .trim(),
+                                    nameOfFile:e.split('.')[0],
+                                    contentOfTxtFile:this._contentOfFile(e)
                                 }                               
                             })
     }
 
     verifyContentOfFile() {
+        logger.info('Verify content of file')
+
         this.relationNameFile.map(e => {
             const filesOfThisName = this.filesPosition.filter(fps => {
                 return fps.includes(e.nameOfFile) && fps.split('.')[1] !== 'txt'
             }).map(e => {
                 return {
                     nameOfFile:e,
-                    sizeOfFile: (fs.statSync(`${this.positionOfFile}/${e}`).size)/(1024*1000) + ' MB'
+                    sizeOfFile: (fs.statSync(`${this.positionOfFile}/${e}`).size)/(1024*1000)
                 }
             })
 
@@ -45,8 +49,24 @@ export default class ProcessElement {
     }
 
     decideSituation() {
-        console.log(this.generalData)
+        logger.info('Decide situation of each student')
+        
+        this.generalData.forEach(e => {
+            let lineFile = `${e.nameOfStudent},`
+
+            if (e.file.length > 0) {
+                lineFile += `1\n`
+            } else {
+                lineFile += `ponto-atencao:\n\n${e.contentOfTxtFile}\n`
+            }
+
+            this.documentContent += lineFile
+        })
+        console.log(this.documentContent)
     }
 
+    _contentOfFile(e) {
+        return fs.readFileSync(`${this.positionOfFile}/${e}`,'utf8')
+    }
 
 }
